feb08c21f7e083cc4c8d320c758d9ad9b5e10517
byzantinist <echiang777@yahoo.com>
2017-11-20 12:57:36 -0600

    Copy over slow_your_scroll files; app works but still need to implement Montserrat font and app icon

---
+++ components/listarticles.js
@@ -0,0 +1,199 @@
+import React, { Component } from 'react';
+import { Card, CardItem } from 'native-base';
+import { Alert, Dimensions, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+var api = {
+  getArticles(){
+    var url = 'https://desolate-oasis-97513.herokuapp.com/scrollios/1'
+    return fetch(url).then((response) => response.json());
+  }
+};
+
+export default class ListArticles extends Component {
+  static navigationOptions = {
+    title: 'List Articles',
+    headerStyle: {
+      backgroundColor: '#36485f'
+    },
+    headerTintColor: 'white'
+  };
+
+  constructor(props){
+    super(props);
+    this.state = {
+      scroll: [],
+      deletion_id: 0
+    }
+  }
+
+  componentWillMount(){
+    api.getArticles().then((response) => {
+      this.setState({
+        scroll: response
+      })
+    })
+  }
+
+  render() {
+    const { navigate } = this.props.navigation;
+    let thisScroll = this.state.scroll.map((article, index) =>
+      <Card style={styles.card} key={index}>
+        <View style={styles.cardView}>
+        <CardItem style={styles.articleTitle}>
+          <Text style={styles.articleTitleText}>{article.title}</Text>
+          <TouchableOpacity style={styles.deleteButton} onPress={() => {
+              this.setState({deletion_id: article.id});
+              this.deleteArticle()
+              }
+            }>
+            <Text style={styles.deleteButtonText}>🗑️</Text>
+          </TouchableOpacity>
+        </CardItem>
+        </View>
+
+      </Card>)
+
+    return (
+      <View style={styles.container}>
+      <View style={styles.imageview}><Image style={styles.image} source={require('../images/rain.jpg')}/></View>
+        <View>
+          <ScrollView>
+            <TouchableOpacity style={styles.button} onPress={() => navigate('ScrollArticles')}>
+              <Text style={styles.buttonText}>Start Scroll</Text>
+            </TouchableOpacity>
+            {thisScroll}
+            <TouchableOpacity style={styles.button} onPress={this.clearArticles}>
+            <Text style={styles.buttonText}>Clear All Articles</Text>
+            </TouchableOpacity>
+          </ScrollView>
+        </View>
+      </View>
+     );
+  }
+
+  clearArticles = () => {
+    Alert.alert(
+      'Are you sure you want to clear all articles?',
+      'Please confirm!',
+      [
+        {text: 'Clear All Articles', onPress: () => {
+          fetch('https://desolate-oasis-97513.herokuapp.com/scrollios/1', {
+              method: 'DELETE',
+              headers: {
+                'Accept' : 'application/json',
+                'Content-Type': 'application/json',
+              },
+          })
+          .then((response) => {
+            Alert.alert(
+              'Success!',
+              'All articles have been cleared!',
+              [{Text: 'OK', onPress: () => {
+                this.setState({scroll: []})
+              }}],
+              { cancelable: false }
+            )
+          })
+          .done();
+        }},
+        {text: 'Cancel', style: 'cancel'}
+      ],
+      { cancelable: false }
+    )
+  }
+
+  deleteArticle = () => {
+    Alert.alert(
+      'Are you sure you want to remove this article?',
+      'Please confirm!',
+      [
+        {text: 'Remove This Article', onPress: () => {
+          var deletion_url = 'https://desolate-oasis-97513.herokuapp.com/scrollios/1/articles/' + this.state.deletion_id;
+          fetch(deletion_url, {
+              method: 'DELETE',
+              headers: {
+                'Accept' : 'application/json',
+                'Content-Type': 'application/json',
+              },
+          })
+          .then((response) => {
+            Alert.alert(
+              'Success!',
+              'This article has been deleted!',
+              [{Text: 'OK', onPress: () => {
+                this.componentWillMount()
+              }}],
+              { cancelable: false }
+            )
+          })
+          .done();
+        }},
+        {text: 'Cancel', style: 'cancel'}
+      ],
+      { cancelable: false }
+    )
+  }
+}
+
+const width = Dimensions.get('window').width;
+const styles = StyleSheet.create({
+  articleTitle: {
+    flex: 1,
+    backgroundColor: 'rgb(222, 228, 237)',
+  },
+  articleTitleText: {
+    width: width/1.3,
+    fontSize: 18,
+  },
+  button: {
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: 'rgba(222, 228, 237, 0.8)',
+    margin: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'black',
+    fontWeight: '300',
+    fontSize: 28,
+  },
+  card: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+  },
+  cardView: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    // flexDirection: 'row',
+    flex: 1,
+  },
+  container: {
+    width: width,
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#36485f',
+  },
+  deleteButton: {
+    flex: 1,
+    width: width/0.8,
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    alignItems: 'center',
+  },
+  deleteButtonText: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  image: {
+    flex: 1,
+  },
+  imageview: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+});
---
+++ components/scrollarticles.js
@@ -0,0 +1,224 @@
+import React, { Component } from 'react';
+import { Container, Header } from 'native-base';
+import { Animated, Button, Dimensions, Easing, Keyboard, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Display from 'react-native-display';
+import { StackNavigator } from 'react-navigation';
+
+Keyboard.dismiss();
+
+var api = {
+  getArticles(){
+    var url = 'https://desolate-oasis-97513.herokuapp.com/scrollios/1'
+    return fetch(url).then((response) => response.json());
+  }
+};
+
+var {
+  width,
+  height
+} = Dimensions.get('window');
+
+var self;
+var referenceIndex = 1;
+
+export default class ScrollArticles extends Component {
+  static navigationOptions = ({ navigation }) => {
+      const { params = {} } = navigation.state;
+      return {
+        title: 'Scroll Articles',
+        headerStyle: {
+          backgroundColor: 'rgba(222, 228, 237, 0.8)'
+        },
+        headerRight: <Button title="Next Article" onPress={() => params.handleNext()} />
+      };
+    };
+
+  _nextArticle() {
+    if (self.state.scroll.length > 0) {
+      if (referenceIndex >= self.state.scroll.length) {
+        referenceIndex = 0;
+      }
+      var referenceName = "ref" + referenceIndex;
+      self.refs[referenceName].focus();
+      Keyboard.dismiss();
+      referenceIndex += 1;
+    }
+  }
+
+  constructor(){
+   super();
+    this.state = {
+      pan: new Animated.ValueXY(),
+      scroll: [],
+      duration: 300000,
+      pause: false,
+      oldSpeed: 0,
+      pauseEnable: true,
+      playEnable: false
+    }
+    self = this;
+  }
+
+  componentWillMount(){
+    this.props.navigation.setParams({ handleNext: this._nextArticle });
+
+    api.getArticles().then((response) => {
+      this.setState({
+        scroll: response
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.triggerAnimation();
+  }
+
+  triggerAnimation(cb) {
+    Animated.sequence([
+      Animated.timing(this.state.pan, {
+      duration: this.state.duration,
+      easing: Easing.linear,
+      toValue: {x: 0, y: -5000}
+      }),
+    ]).start(cb);
+  }
+
+  getStyle() {
+    return [
+    styles.square,
+      {
+        transform: this.state.pan.getTranslateTransform(),
+        marginTop: height/2.8,
+      }
+    ];
+  }
+
+  onLayoutFigureOutHeight(event) {
+    var {y, width, x, height} = event.nativeEvent.layout;
+  }
+
+  render() {
+    let scrollData = (
+      <Animated.View style={this.getStyle()}>
+        {this.state.scroll.map((article, reference) =>
+          <View style={styles.newsArticle} key={article.id}>
+            <TextInput ref={"ref" + reference}/>
+            <Text style={styles.title}>{article.title}</Text>
+             <View>{article.body.map((para, index) =>
+               <Text style={styles.paragraph} key={index}>{para}</Text>)}
+            </View>
+            <View style={{backgroundColor:'#36485f',flex:1,height:50}}/>
+          </View>)
+        }
+      </Animated.View>)
+    return (
+      <Container>
+        <Header style={styles.header}>
+
+          <TouchableOpacity style={styles.speedButton} onPress={() => {
+            if (!this.state.pause) {
+              var newSpeed = this.state.pan.y._animation._toValue * 0.8;
+              var newOffset = this.state.pan.y._value * 0.8;
+              this.state.pan.y._animation._toValue = newSpeed;
+              this.state.pan.y._offset = -1 * newOffset;
+            }
+          }
+          }>
+          <Text style={styles.buttonText}> 🐢</Text>
+        </TouchableOpacity>
+
+        <Container style={styles.speedBar}>
+
+          <TouchableOpacity style={styles.speedButton} onPress={() => {
+              if (this.state.pause) {
+                  this.setState({
+                    pause: false,
+                    pauseEnable: true,
+                    playEnable: false
+                  });
+                  this.triggerAnimation();
+                  this.state.pan.y._animation._toValue = this.state.oldSpeed;
+                }
+                else {
+                  this.setState({
+                    pause: true,
+                    oldSpeed: this.state.pan.y._animation._toValue,
+                    pauseEnable: false,
+                    playEnable: true
+                  });
+                  this.state.pan.stopAnimation(this.callback);
+                }
+              }
+          }>
+          <Display enable={this.state.pauseEnable}>
+            <Text style={styles.buttonText}> ⏸️</Text>
+          </Display>
+          <Display enable={this.state.playEnable}>
+            <Text style={styles.buttonText}> ▶️</Text>
+          </Display>
+
+        </TouchableOpacity>
+
+        </Container>
+
+          <TouchableOpacity style={styles.speedButton} onPress={() => {
+              if (!this.state.pause) {
+                var newSpeed = this.state.pan.y._animation._toValue * 1.2;
+                var newOffset = this.state.pan.y._value * 1.2;
+                this.state.pan.y._animation._toValue = newSpeed;
+                this.state.pan.y._offset = -1 * newOffset;
+              }
+            }
+          }>
+            <Text style={styles.buttonText}>🐇 </Text>
+          </TouchableOpacity>
+        </Header>
+
+        <ScrollView style={styles.container} ref="autoScroll">{scrollData}</ScrollView>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  buttonText: {
+    fontSize: 35,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#36485f',
+  },
+  header: {
+    backgroundColor: '#36485f',
+    borderBottomWidth: 0,
+  },
+  newsArticle : {
+    marginBottom: 30,
+  },
+  paragraph: {
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingLeft: 20,
+    paddingRight: 20,
+    fontSize: 21,
+    color: '#222'
+  },
+  speedBar: {
+    alignItems: 'center',
+  },
+  speedButton: {
+    backgroundColor: '#36485f',
+  },
+  square: {
+    width: width,
+    backgroundColor: '#dee4ed'
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    marginBottom: 10,
+    fontWeight: 'bold',
+    color: '#222',
+    padding: 15
+  },
+});
